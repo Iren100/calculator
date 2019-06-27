8bf252d991d18139ebf228e5d79c2cee4061f2fe
Iren100 <32511097+Iren100@users.noreply.github.com>
2019-06-27 12:47:32 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -60,12 +60,11 @@ Str = (l = 15) => {
 function DNull(){
 	const ARR = ["","-","+","*","/"];
 	let el0 = CALC_DISPLAY.value.charAt(CALC_DISPLAY.value.length - 1);
-	if (el0 != 0) return;
 	let el1 = CALC_DISPLAY.value.charAt(CALC_DISPLAY.value.length - 3);
 	let el2 = CALC_DISPLAY.value.charAt(CALC_DISPLAY.value.length - 2);
 	if  ((ARR.indexOf(el1) != -1) && (el2 ==="0"))
 		CALC_DISPLAY.value = 
-			CALC_DISPLAY.value.substring(0, CALC_DISPLAY.value.length - 1);
+			CALC_DISPLAY.value.substring(0, CALC_DISPLAY.value.length - 2) + el0;
 }
 
 function TimeOut(){
